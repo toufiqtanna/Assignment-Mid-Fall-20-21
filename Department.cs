60a5389be6baf92aba43e76437ce942d1a977ac4
toufiqtanna <toufiqtanna@gmail.com>
2020-11-20 00:02:12 +0600

    1st

---
+++ Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid_Assignment
+{
+    class Department
+    {
+        private string deptName;
+        public string departmentName
+        {
+            get { return deptName; }
+            set { deptName = value; }
+        }
+   
+
+    public Department(string deptName)
+    {
+        this.deptName = deptName;
+    }
+    public void showInfo()
+    {
+        Console.WriteLine("This is Department "+deptName);
+    }
+
+    }
+}

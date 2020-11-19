34cc2f8634194986cef8a4471f982ffe960e15a1
toufiqtanna <toufiqtanna@gmail.com>
2020-11-20 00:03:36 +0600

    1st

---
+++ Section.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid_Assignment
+{
+    class Section
+    {
+        private string s_Name;
+        public string SectionName
+        {
+            get;set;
+        }
+        public Section(string s_Name)
+        {
+            this.s_Name=s_Name;
+        }
+        public void showInfo()
+        {
+            Console.WriteLine("Section: " + s_Name);
+        }
+    }
+}
